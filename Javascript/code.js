a11cf4602552ccfbe2c48cf0a1c319d5e34d4973
GSamuels5 <ghamzahsamuels23@gmail.com>
2023-11-20 15:08:27 +0200

    uncomplete data

---
+++ Javascript/code.js
@@ -0,0 +1,9 @@
+let name = document.querySelector('input')
+let date = document.querySelector('[data-date]')
+let button = document.querySelector('button')
+
+birthDay = newDate(date)
+
+const currentDate = newDate()
+const numbOfDays = difference / (1000 * 3600 * 24)
+// Difference between dates
